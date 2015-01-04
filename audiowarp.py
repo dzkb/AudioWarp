cec0ba048a219dd6037b83298dfeff1290e9b26c
Dzakub <dzakub@gmail.com>
2015-01-04 12:56:28 +0100

    Bugfixes, merging into separate files
    Todo: merge into one file
    record separate devices

--- audiowarp.py
+++ audiowarp.py
@@ -46,6 +46,10 @@ TMP_EXTENSION = ".tmp"
 THREAD_COUNT = 2 # one for OS audio, one for the microphone
 
 threads = dict()
+events = dict()
+counters = dict()
+finishedMergingEvent = threading.Event()
+finishedMergingEvent.set()
 mergingLock = threading.Lock()
 merge_and_save = False
 shallQuit = False
@@ -57,38 +61,45 @@ f_counters = dict() # contains buffer files counters
 +--+--+--+--+--+
 '''
 
-def mergeAndSave(counter, buffers, channels, iformat, rate, thread_id):
-    print("[" + str(thread_id) + "] " + "counter is " + str(counter))
-    print("[" + str(thread_id) + "] " + "counter - buffers is " + str(counter-buffers))
+def mergeAndSave(counters, thread_id): # counter -> counters (dict)
+    global merge_and_save
+    for k, v in counters.items():
+        print("[" + str(thread_id) + "/" + str(k) + "] " + "counter is " + str(v))
+        print("[" + str(thread_id) + "] " + "counter - buffers is " + str(v-BUFFERS))
 
-    wavedata = []
-    for i in range(buffers-counter):
-        filename = str(i+counter) + TMP_EXTENSION
-        print("[" + str(thread_id) + "] " + "(1) merging file " + filename)
-        if os.path.isfile(filename):
-            f = wave.open(filename, "rb")
-            wavedata.append(f.readframes(f.getnframes()))
-            f.close()
-    for i in range(counter):
-        filename = str(i) + TMP_EXTENSION
-        print("[" + str(thread_id) + "] " + " (2) merging file " + filename)
-        if os.path.isfile(filename):
-            f = wave.open(filename, "rb")
-            wavedata.append(f.readframes(f.getnframes()))
-            f.close()
+        wavedata = []
+        for i in range(BUFFERS-v):
+            filename = str(i+v) + "_" + str(k) + TMP_EXTENSION
+            print("[" + str(thread_id) + "] " + "(1) merging file " + filename)
+            if os.path.isfile(filename):
+                f = wave.open(filename, "rb")
+                wavedata.append(f.readframes(f.getnframes()))
+                f.close()
+        for i in range(v):
+            filename = str(i) + "_" + str(k) + TMP_EXTENSION
+            print("[" + str(thread_id) + "] " + " (2) merging file " + filename)
+            if os.path.isfile(filename):
+                f = wave.open(filename, "rb")
+                wavedata.append(f.readframes(f.getnframes()))
+                f.close()
 
-    # outName = time.strftime("%Y-%m-%d_%Hh%Mm%Ss") + ".wav"
-    outName = "out_" + str(thread_id)
+        # outName = time.strftime("%Y-%m-%d_%Hh%Mm%Ss") + ".wav"
+        outName = "out_" + str(k)
 
-    out = wave.open(outName, "wb")
-    out.setnchannels(channels)
-    out.setsampwidth(p.get_sample_size(iformat))
-    out.setframerate(rate)
-    out.writeframes(b''.join(wavedata))
-    out.close()
+        out = wave.open(outName, "wb")
+        out.setnchannels(CHANNELS)
+        out.setsampwidth(p.get_sample_size(FORMAT))
+        out.setframerate(RATE)
+        out.writeframes(b''.join(wavedata))
+        out.close()
+
+    merge_and_save = False
+    finishedMergingEvent.set()
 
 def recordingThread(thread_id):
     global merge_and_save
+    global events
+    global counters # should be safe
     f_counter = 0
 
     for k,v in enumerate(p.get_device_info_by_index(2).items()):
@@ -128,20 +139,26 @@ def recordingThread(thread_id):
         else:
             f_counter = 0
 
-        if merge_and_save:
-            mergeAndSave(f_counter, BUFFERS, CHANNELS, FORMAT, RATE, thread_id)
-            mergingLock.acquire()
-            merge_and_save = False
-            mergingLock.release()
+        if merge_and_save: # moving to another thread
+            counters[thread_id] = f_counter 
+            events[thread_id].set()
+            finishedMergingEvent.wait()
+            print("[" + str(thread_id) + "] " + "after a wait")
+            # mergeAndSave(f_counter, BUFFERS, CHANNELS, FORMAT, RATE, thread_id)
+            # mergingLock.acquire()
+            # merge_and_save = False
+            # mergingLock.release()
 
     print("[" + str(thread_id) + "] " + "terminating...")
     stream.stop_stream()
     stream.close()
 
 for i in range(THREAD_COUNT): # create one worker thread for every audio device
-    t = threading.Thread(target=recordingThread, daemon=True, kwargs={'thread_id': i})
+    t = threading.Thread(target=recordingThread, daemon=True, kwargs={'thread_id' : i})
+    e = threading.Event()
     t.start()
     threads[i] = t
+    events[i] = e
     print ("Thread " + str(i) + " started")
 
 def stopThat() :
@@ -153,12 +170,19 @@ def stopThat() :
         print("[MAIN] Joining thread " + str(k))
     sys.exit(0)
 
-def startMerging():
+def startMerging(): # merging is moved to another thread
     print("[MAIN] Requested save")
+    finishedMergingEvent.clear()
+    # ADD args, fix counter
+    t = threading.Thread(target=mergeAndSave, daemon=True, kwargs={'thread_id': "MERGE", 'counters' : counters}) # missing args
     global merge_and_save
     mergingLock.acquire()
     merge_and_save = True
     mergingLock.release()
+    for k,v in events.items():
+        v.wait() # wait for all events to become ready
+        v.clear()
+    t.start() # start merging
 
 user32.RegisterHotKey(None, 1, win32con.MOD_WIN, win32con.VK_F3)
 user32.RegisterHotKey(None, 2, win32con.MOD_WIN, win32con.VK_ESCAPE)
